2c6687b606f3848afee70383d75b305625645589
JUDO1-86 <gv7730144@gmail.com>
2025-03-19 01:21:31 +0530

    backend.py

--- GUI_Assembler/backend.py
+++ GUI_Assembler/backend.py
@@ -209,5 +209,6 @@ def assemble():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
